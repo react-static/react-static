6bb8af81c8950e14dd66e21cc996b1d7579f3fac
Jason Maurer <j@maur.co>
2017-10-05 20:02:54 -0600

    fix for missing public folder

--- src/copyPublicFolder.js
+++ src/copyPublicFolder.js
@@ -2,6 +2,8 @@ import fs from 'fs-extra'
 import { PUBLIC, INDEX } from './paths'
 
 export default function copyPublicFolder (dest) {
+  fs.ensureDirSync(PUBLIC)
+
   fs.copySync(PUBLIC, dest, {
     dereference: true,
     filter: file => file !== INDEX,
