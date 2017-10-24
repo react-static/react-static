f598a8cb6680105d28ba23f39f240c9f59c8912f
Tanner Linsley <tannerlinsley@gmail.com>
2017-10-24 10:31:34 -0600

    Do not stash react-static dependecy in example packages. Use fresh install at project creation.

---
+++ examples/custom-routing/README.md
@@ -0,0 +1,9 @@
+# React-Static - Custom Routing Example
+
+This example includes:
+- CSS imports
+- Image imports
+- File imports
+- Custom Routing
+
+To get started, run `react-static create` and use the `custom-routing` template.
--- src/commands/create.js
+++ src/commands/create.js
@@ -38,28 +38,26 @@ export default async () => {
 
   // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
   // See: https://github.com/npm/npm/issues/1862
-  fs.move(
-    path.join(dest, 'gitignore'),
-    path.join(dest, '.gitignore'),
-    [],
-    err => {
-      if (err) {
-        // Append if there's already a `.gitignore` file there
-        if (err.code === 'EEXIST') {
-          const data = fs.readFileSync(path.join(dest, 'gitignore'))
-          fs.appendFileSync(path.join(dest, '.gitignore'), data)
-          fs.unlinkSync(path.join(dest, 'gitignore'))
-        } else {
-          throw err
-        }
+  fs.move(path.join(dest, 'gitignore'), path.join(dest, '.gitignore'), [], err => {
+    if (err) {
+      // Append if there's already a `.gitignore` file there
+      if (err.code === 'EEXIST') {
+        const data = fs.readFileSync(path.join(dest, 'gitignore'))
+        fs.appendFileSync(path.join(dest, '.gitignore'), data)
+        fs.unlinkSync(path.join(dest, 'gitignore'))
+      } else {
+        throw err
       }
-    },
-  )
+    }
+  })
 
   const isYarn = shouldUseYarn()
 
   console.log('=> Installing dependencies...')
-  execSync(`cd ${answers.name} && ${isYarn ? 'yarn' : 'npm install'}`)
+  execSync(`cd ${answers.name}`)
+  execSync(`${isYarn ? 'yarn' : 'npm install'}`)
+  // We install react-static separately to ensure we always have the latest stable release
+  execSync(`${isYarn ? 'yarn add react-static' : 'npm install react-static --save'}`)
   console.log('')
   console.timeEnd(chalk.green(`=> [\u2713] Project "${answers.name}" created`))
 
