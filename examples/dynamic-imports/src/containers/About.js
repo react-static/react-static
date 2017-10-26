8d3265d4eb9b144d6bd35daf5a7610dd2daf91ed
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2017-10-26 20:29:18 +0200

    Documentation

--- examples/dynamic-imports/src/containers/About.js
+++ examples/dynamic-imports/src/containers/About.js
@@ -19,7 +19,12 @@ export default class About extends React.Component {
     super(...args);
     this.state = { MyComponent: null };
   }
+  ///////////////////////////////////////////////////////////////////////////
   // // Option 1
+  // // Behaviour:
+  // //   - Loaded component on static build
+  // //   - UnLoaded component when react rehydrates
+  // //   - Loaded component when dynamic import resolves
   // componentWillMount() {
   //   if (typeof document === 'undefined') {
   //     const { MyComponent } = require('../components/MyComponent')
@@ -30,12 +35,18 @@ export default class About extends React.Component {
   //       .catch(error => this.setState({ MyComponent: Failed }))
   //   }
   // }
-  //Option 2
+  ///////////////////////////////////////////////////////////////////////////
+  // Option 2
+  // Behaviour:
+  //   - UnLoaded component on static build
+  //   - UnLoaded component when react rehydrates
+  //   - Loaded component when dynamic import resolves
   componentWillMount() {
     import("../components/MyComponent")
       .then(({ MyComponent }) => this.setState({ MyComponent }))
       .catch(error => this.setState({ MyComponent: Failed }));
   }
+  ///////////////////////////////////////////////////////////////////////////
   render() {
     const { MyComponent } = this.state;
     if (MyComponent !== null) {
