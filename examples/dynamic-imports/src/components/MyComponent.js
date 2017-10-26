9027d38f21146013029ead26922a37a00d2f314f
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2017-10-27 00:46:56 +0200

    Add dynamic imports example (#98)
    * Add dynamic imports example
    
    * Documentation
    
    * Format
    
    * Format
    
    * Update README.md
    
    * Update static.config.js

---
+++ examples/dynamic-imports/src/components/MyComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MyComponent = () => (
+  <div style={{ color: 'green' }}>
+    <h1>Heavy component loaded !</h1>
+  </div>
+)
---
+++ examples/dynamic-imports/static.config.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+//
+
+export default {
+  getSiteProps: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getProps: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getProps: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+}
