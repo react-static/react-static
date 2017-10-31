9fbbecd60e202e9d92cf9e07e6c868911bdeda28
Diego Lameira <diegojgl@gmail.com>
2017-10-31 14:52:16 -0200

    Using PORT from process.env instead, if it exists (#109)
    * Using PORT from process.env instead, if it exists
    
    * Update configServer.js

--- src/configServer.js
+++ src/configServer.js
@@ -7,7 +7,7 @@ import { findAvailablePort, getConfig } from './utils'
 export async function startConfigServer () {
   const config = getConfig()
   // scan a range
-  const port = await findAvailablePort(8000)
+  const port = process.env.PORT || await findAvailablePort(8000)
   process.env.STATIC_ENDPOINT = `http://127.0.0.1:${port}`
 
   const configApp = express()
