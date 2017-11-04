b39ce8f80f666d6b11b14fcd43352c41d8f53a52
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2017-11-04 12:21:05 +0100

    Add apollo redux example

---
+++ examples/apollo-redux/static.config.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+export default {
+  getSiteProps: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getProps: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getProps: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+  // webpack: (config, { defaultLoaders }) => {
+  //   config.module.rules = [
+  //     {
+  //       oneOf: [
+  //         {
+  //           test: /\.json$/,
+  //           use: [{ loader: 'json-loader' }],
+  //         },
+  //         defaultLoaders.jsLoader,
+  //         defaultLoaders.cssLoader,
+  //         defaultLoaders.fileLoader,
+  //       ],
+  //     },
+  //   ]
+  //   return config
+  // },
+}
