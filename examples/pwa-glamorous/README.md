e2e2e1886083cd1c6115b6fa738fcb3bc94bd931
Louis Galipeau <louis@muloka.com>
2017-11-24 22:55:47 -0400

    Copied glamorous to pwa-glamorous

---
+++ examples/pwa-glamorous/README.md
@@ -0,0 +1,9 @@
+# React-Static - Glamorous Example
+
+This example includes:
+- Glamorous
+- Image imports
+- File imports
+- Automatic Routing
+
+To get started, run `react-static create` and use the `glamorous` template.
---
+++ examples/pwa-glamorous/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Router, Link } from 'react-static'
+import glamorous from 'glamorous'
+//
+import Routes from 'react-static-routes'
+
+import './app.css'
+
+const AppStyles = glamorous.div({
+  fontFamily:
+    "'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif",
+  fontWeight: '300',
+  fontSize: '16px',
+  margin: '0',
+  padding: '0',
+  '& a': {
+    textDecoration: 'none',
+    color: '#108db8',
+    fontWeight: 'bold',
+  },
+  '& nav': {
+    width: '100%',
+    background: '#108db8',
+    '& a': {
+      color: 'white',
+      padding: '1rem',
+      display: 'inline-block',
+    },
+  },
+  '& .content': {
+    padding: '1rem',
+  },
+})
+
+export default () => (
+  <Router>
+    <AppStyles>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+      <div className="content">
+        <Routes />
+      </div>
+    </AppStyles>
+  </Router>
+)
