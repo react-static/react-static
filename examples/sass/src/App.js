0567a78264e6a3c566d11a0e0768c89860f017b1
Andrew Carr <andrew@2metres.com>
2017-12-02 15:00:12 +1100

    Sass example

---
+++ examples/sass/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Router, Link } from 'react-static'
+//
+import Routes from 'react-static-routes'
+
+import './app.scss'
+
+export default () => (
+  <Router>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  </Router>
+)
