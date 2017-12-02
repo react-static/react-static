1d9879c7e5d9c76723bb7b6f753eec4c9327d2a2
Andrew Carr <2metres@users.noreply.github.com>
2017-12-02 16:17:36 +1100

    Update About.js

--- examples/sass/src/containers/About.js
+++ examples/sass/src/containers/About.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>This is what we‘re all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+    <h1>This is what we’re all about.</h1>
+    <p>React, static sites, performance, speed. It’s the stuff that makes us tick.</p>
   </div>
 )
