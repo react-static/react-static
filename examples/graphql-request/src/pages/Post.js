7d8ec33bf333e4dd8a505a602d3068926fb34787
hmeissner <hugomeissner@icloud.com>
2017-12-03 16:11:30 +0100

    avoid eslint react/no-danger warning with proper explanation

--- examples/graphql-request/src/pages/Post.js
+++ examples/graphql-request/src/pages/Post.js
@@ -10,6 +10,12 @@ export default getRouteProps(({ post }) => (
         src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.coverImage.handle}`}
       />
     </div>
-    <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    {/*
+      dangerouslySetInnerHTML is here to parse a markdown field from GraphCMS.
+      The field contains a string with HTML tags as GraphCMS already does the markdown parsing.
+      If you want to avoid dangerouslySetInnerHTML in your project you can use any html/markdown
+      parsing plugin for React like https://github.com/rexxars/react-markdown
+    */}
+    <div dangerouslySetInnerHTML={{ __html: post.content }} /> {/* eslint react/no-danger: 0 */}
   </article>
 ))
