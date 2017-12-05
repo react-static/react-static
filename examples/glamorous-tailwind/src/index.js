6448f4f1ebdd3c8f722a898a4e492359afd23edc
Deden Ramadhan <deden96@gmail.com>
2017-12-05 18:25:55 +0700

    Use ReactDOM.render

--- examples/glamorous-tailwind/src/index.js
+++ examples/glamorous-tailwind/src/index.js
@@ -5,13 +5,14 @@ import { AppContainer } from 'react-hot-loader'
 // Your top level component
 import App from './App'
 
-// Export your top level component (for static rendering)
+// Export your top level component as JSX (for static rendering)
 export default App
 
 // Render your app
 if (typeof document !== 'undefined') {
+  const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate
   const render = Comp => {
-    ReactDOM.hydrate(
+    renderMethod(
       <AppContainer>
         <Comp />
       </AppContainer>,
