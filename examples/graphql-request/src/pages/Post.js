c90afc164df5a2067442f8fbb63e9e023b486a29
Hugo Meissner <hugomeissner@icloud.com>
2017-12-08 16:41:03 +0100

    Update graphql-request example to use react-markdown (#227)
    * change gql-request to markdown
    
    * little fix

--- examples/graphql-request/src/pages/Post.js
+++ examples/graphql-request/src/pages/Post.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getRouteProps } from 'react-static'
+import Markdown from 'react-markdown'
 
 export default getRouteProps(({ post }) => (
   <article>
@@ -10,12 +11,9 @@ export default getRouteProps(({ post }) => (
         src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.coverImage.handle}`}
       />
     </div>
-    {/*
-      dangerouslySetInnerHTML is here to parse a markdown field from GraphCMS.
-      The field contains a string with HTML tags as GraphCMS already does the markdown parsing.
-      If you want to avoid dangerouslySetInnerHTML in your project you can use any html/markdown
-      parsing plugin for React like https://github.com/rexxars/react-markdown
-    */}
-    <div dangerouslySetInnerHTML={{ __html: post.content }} /> {/* eslint react/no-danger: 0 */}
+    <Markdown
+      source={post.content}
+      escapeHtml={false}
+    />
   </article>
 ))
