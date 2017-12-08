241259fed1a6ff04bbbb204bdf228d32a215b2f2
Deden Ramadhan <deden96@gmail.com>
2017-12-09 03:50:16 +0700

    add glamorous-tailwind example (#218)
    * add glamorous-tailwind example
    
    * package.json updated
    
    * Use ReactDOM.render
    
    * no-undef var explicitly mentioned in global
    
    * adjust styling to pass Travis

---
+++ examples/glamorous-tailwind/static.config.js
@@ -0,0 +1,62 @@
+/* eslint-disable react/no-danger */
+import axios from 'axios'
+import React, { Component } from 'react'
+import { renderStaticOptimized } from 'glamor/server'
+
+export default {
+  getRoutes: async () => {
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getProps: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getProps: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+  renderToHtml: async (render, Comp, meta) => {
+    const html = render(<Comp />)
+    const { css } = renderStaticOptimized(() => html)
+    meta.glamStyles = css
+    return html
+  },
+  Document: class CustomDocument extends Component {
+    render () {
+      const { Html, Head, Body, children, renderMeta } = this.props
+
+      return (
+        <Html>
+          <Head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <style dangerouslySetInnerHTML={{ __html: renderMeta.glamStyles }} />
+          </Head>
+          <Body>
+            {children}
+          </Body>
+        </Html>
+      )
+    }
+  },
+}
