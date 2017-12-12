ed99a40a02dee04331c6fbabdfde45f35590f882
Tanner Linsley <tannerlinsley@gmail.com>
2017-12-12 16:37:50 -0700

    Pass Linting

--- examples/sass/static.config.js
+++ examples/sass/static.config.js
@@ -1,42 +1,40 @@
-import axios from "axios";
-import ExtractTextPlugin from "extract-text-webpack-plugin";
+import axios from 'axios'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   getSiteProps: () => ({
-    title: "React Static"
+    title: 'React Static',
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
     return [
       {
-        path: "/",
-        component: "src/containers/Home"
+        path: '/',
+        component: 'src/containers/Home',
       },
       {
-        path: "/about",
-        component: "src/containers/About"
+        path: '/about',
+        component: 'src/containers/About',
       },
       {
-        path: "/blog",
-        component: "src/containers/Blog",
+        path: '/blog',
+        component: 'src/containers/Blog',
         getProps: () => ({
-          posts
+          posts,
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
-          component: "src/containers/Post",
+          component: 'src/containers/Post',
           getProps: () => ({
-            post
-          })
-        }))
+            post,
+          }),
+        })),
       },
       {
         is404: true,
-        component: "src/containers/404"
-      }
-    ];
+        component: 'src/containers/404',
+      },
+    ]
   },
   webpack: (config, { defaultLoaders, stage }) => {
     config.module.rules = [
@@ -45,35 +43,31 @@ export default {
           {
             test: /\.s(a|c)ss$/,
             use:
-              stage === "dev"
-                ? [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                  ]
+              stage === 'dev'
+                ? [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
                 : ExtractTextPlugin.extract({
-                    use: [
-                      {
-                        loader: "css-loader",
-                        options: {
-                          importLoaders: 1,
-                          minimize: true,
-                          sourceMap: false
-                        }
+                  use: [
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: false,
                       },
-                      {
-                        loader: "sass-loader",
-                        options: { includePaths: "src/" }
-                      }
-                    ]
-                  })
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: { includePaths: 'src/' },
+                    },
+                  ],
+                }),
           },
           defaultLoaders.cssLoader,
           defaultLoaders.jsLoader,
-          defaultLoaders.fileLoader
-        ]
-      }
-    ];
-    return config;
-  }
-};
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
+  },
+}
