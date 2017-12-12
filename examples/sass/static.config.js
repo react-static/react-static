83edb104ba558d4d47fb16f94e2b12df3abed695
Tanner Linsley <tannerlinsley@gmail.com>
2017-12-12 16:21:37 -0700

    Reformat changes

--- examples/sass/static.config.js
+++ examples/sass/static.config.js
@@ -1,81 +1,79 @@
-import axios from 'axios'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
-
-const SASS_INCLUDE_PATHS = ['src/']
-
-let sassUse = []
-if (process.env.NODE_ENV === 'development') {
-  sassUse = [
-    { loader: 'style-loader' },
-    { loader: 'css-loader' },
-    { loader: 'sass-loader' },
-  ]
-} else {
-  sassUse = ExtractTextPlugin.extract({
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-          minimize: true,
-          sourceMap: false,
-        },
-      },
-      {
-        loader: 'sass-loader',
-        options: { includePaths: SASS_INCLUDE_PATHS },
-      },
-    ],
-  })
-}
+import axios from "axios";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
 
 export default {
   getSiteProps: () => ({
-    title: 'React Static',
+    title: "React Static"
   }),
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home"
       },
       {
-        path: '/about',
-        component: 'src/containers/About',
+        path: "/about",
+        component: "src/containers/About"
       },
       {
-        path: '/blog',
-        component: 'src/containers/Blog',
+        path: "/blog",
+        component: "src/containers/Blog",
         getProps: () => ({
-          posts,
+          posts
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
-          component: 'src/containers/Post',
+          component: "src/containers/Post",
           getProps: () => ({
-            post,
-          }),
-        })),
+            post
+          })
+        }))
       },
       {
         is404: true,
-        component: 'src/containers/404',
-      },
-    ]
+        component: "src/containers/404"
+      }
+    ];
   },
-  webpack: (config, { defaultLoaders }) => {
-    config.module.rules = [{
-      oneOf: [
-        {
-          test: /\.s(a|c)ss$/,
-          use: sassUse,
-        },
-        defaultLoaders.cssLoader,
-        defaultLoaders.jsLoader,
-        defaultLoaders.fileLoader,
-      ],
-    }]
-    return config
-  },
-}
+  webpack: (config, { defaultLoaders, stage }) => {
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use:
+              stage === "dev"
+                ? [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                  ]
+                : ExtractTextPlugin.extract({
+                    use: [
+                      {
+                        loader: "css-loader",
+                        options: {
+                          importLoaders: 1,
+                          minimize: true,
+                          sourceMap: false
+                        }
+                      },
+                      {
+                        loader: "sass-loader",
+                        options: { includePaths: "src/" }
+                      }
+                    ]
+                  })
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader
+        ]
+      }
+    ];
+    return config;
+  }
+};
