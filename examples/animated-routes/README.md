e682fb2302676042fce664fa8323021b7fe5f1a6
Tanner Linsley <tannerlinsley@gmail.com>
2017-12-14 11:37:42 -0700

    Update animated-routes example

--- examples/animated-routes/README.md
+++ examples/animated-routes/README.md
@@ -1,10 +1,14 @@
-# React-Static - Basic Example
+# React-Static - Animated Routes Example
 
-This example is the most basic version of react-static available. It includes:
-- Babel
-- CSS imports
-- Image imports
-- File imports
+This example is a fork of the `basic` template, and demonstrates how to animate routes with:
 - Automatic Routing
+- Routes Render Props
+- React Move
 
-To get started, run `react-static create` and use the `basic` template.
+To get started in the CLI, run `react-static create` and use the `animated-routes` template.
+To get started after cloning this repo:
+  1. Rename `gitignore` to `.gitignore`
+  2. Install deps with `yarn` or `npm install`
+  3. Run the dev server with `yarn start` or `npm run start`
+  4. Build the site with `yarn build` or `npm run build`
+  5. Test the build with `yarn serve` or `npm run serve`
--- examples/animated-routes/src/App.js
+++ examples/animated-routes/src/App.js
@@ -10,96 +10,104 @@ import Routes from 'react-static-routes'
 import './app.css'
 
 // The magic :)
-const AnimatedRoutes = (
-  <Routes
-    component={getContext({
-      // We have to preserve the router context for each route
-      // otherwise, a component may rerender with the wrong data
-      // during animation
-      router: PropTypes.object,
-    })(({ getComponentForPath, router }) => (
-      <Route
-        path="*"
-        render={props => {
-          // Get the component for this path
-          let Comp = getComponentForPath(props.location.pathname)
-          if (!Comp) {
-            Comp = getComponentForPath('404')
-          }
+const AnimatedRoutes = getContext({
+  // We have to preserve the router context for each route
+  // otherwise, a component may rerender with the wrong data
+  // during animation
+  router: PropTypes.object,
+  // We'll also look for the staticURL, so we can disable the animation during static render
+  staticURL: PropTypes.string,
+})(({ getComponentForPath, router, staticURL }) => (
+  <Route
+    path="*"
+    render={props => {
+      // Get the component for this path
+      let Comp = getComponentForPath(props.location.pathname)
+      if (!Comp) {
+        Comp = getComponentForPath('404')
+      }
 
-          // Use React-Move to animate the different components coming in and out
-          return (
-            <NodeGroup
-              // React-move will handle the entry and exit of any items we pass in `data`
-              data={[
-                {
-                  // pass the current Comp, props, ID and router
-                  id: props.location.pathname,
-                  Comp,
-                  props,
-                  router,
-                },
-              ]}
-              keyAccessor={d => d.id}
-              start={() => ({
-                opacity: [0],
-                scale: 1,
-                translateY: [10],
-              })}
-              enter={() => ({
-                opacity: [1],
-                translateY: [0],
-                timing: { duration: 200, delay: 200, ease: easeQuadOut },
-              })}
-              update={() => ({
-                opacity: [1],
-              })}
-              leave={() => ({
-                opacity: [0],
-                translateY: [-10],
-                timing: { duration: 200, ease: easeQuadOut },
-              })}
-            >
-              {nodes => (
-                <div style={{ position: 'relative' }}>
-                  {nodes.map(({ key, data, state: { opacity, translateY } }) => {
-                    // Here, we override the router context with the one that was
-                    // passed with each route
-                    const PreservedRouterContext = withContext(
-                      {
-                        router: PropTypes.object,
-                      },
-                      () => ({
-                        router: data.router,
-                      }),
-                    )(props => <div {...props} />)
+      // When we're rendering for static HTML, be sure to NOT animate in.
+      if (staticURL) {
+        return (
+          // This relative wrapper is necessary for accurate rehydration :)
+          <div style={{ position: 'relative' }}>
+            <Comp {...props} />
+          </div>
+        )
+      }
 
-                    return (
-                      <PreservedRouterContext
-                        key={key}
-                        style={{
-                          position: 'absolute',
-                          top: 0,
-                          right: 0,
-                          bottom: 0,
-                          left: 0,
-                          transform: `translateY(${translateY}px)`,
-                          opacity,
-                        }}
-                      >
-                        <data.Comp {...data.props} />
-                      </PreservedRouterContext>
-                    )
-                  })}
-                </div>
-              )}
-            </NodeGroup>
-          )
-        }}
-      />
-    ))}
+      // Use React-Move to animate the different components coming in and out
+      return (
+        <NodeGroup
+          // React-move will handle the entry and exit of any items we pass in `data`
+          data={[
+            {
+              // pass the current Comp, props, ID and router
+              id: props.location.pathname,
+              Comp,
+              props,
+              router,
+            },
+          ]}
+          keyAccessor={d => d.id}
+          start={() => ({
+            opacity: [0],
+            scale: 1,
+            translateY: [10],
+          })}
+          enter={() => ({
+            opacity: [1],
+            translateY: [0],
+            timing: { duration: 200, delay: 200, ease: easeQuadOut },
+          })}
+          update={() => ({
+            opacity: [1],
+          })}
+          leave={() => ({
+            opacity: [0],
+            translateY: [-10],
+            timing: { duration: 200, ease: easeQuadOut },
+          })}
+        >
+          {nodes => (
+            <div style={{ position: 'relative' }}>
+              {nodes.map(({ key, data, state: { opacity, translateY } }) => {
+                // Here, we override the router context with the one that was
+                // passed with each route
+                const PreservedRouterContext = withContext(
+                  {
+                    router: PropTypes.object,
+                  },
+                  () => ({
+                    router: data.router,
+                  }),
+                )(props => <div {...props} />)
+
+                return (
+                  <PreservedRouterContext
+                    key={key}
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                      bottom: 0,
+                      left: 0,
+                      transform: `translateY(${translateY}px)`,
+                      opacity,
+                    }}
+                  >
+                    <data.Comp {...data.props} />
+                  </PreservedRouterContext>
+                )
+              })}
+            </div>
+          )}
+        </NodeGroup>
+      )
+    }}
   />
-)
+))
 
 export default () => (
   <Router>
@@ -110,7 +118,7 @@ export default () => (
         <Link to="/blog">Blog</Link>
       </nav>
       <div className="content">
-        <AnimatedRoutes />
+        <Routes component={AnimatedRoutes} />
       </div>
     </div>
   </Router>
