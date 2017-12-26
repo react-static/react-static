abbf540cbac72cd5838e448b14d5430d9f295f3b
Cory Deppen <cory@deppen.us>
2017-12-25 20:19:07 -0500

    Add entry for tsx component
    - Migrate remaining components to TS
    - Formatted using Prettier (consistent with react-tools and airbnb ESLint rules)

--- examples/typescript/src/containers/Home.tsx
+++ examples/typescript/src/containers/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
-//
+
 import logoImg from '../logo.png'
 
 export default getSiteProps(() => (
---
+++ examples/typescript/src/types.ts
@@ -0,0 +1,5 @@
+export interface Post {
+  body: string
+  id: number
+  title: string
+}
