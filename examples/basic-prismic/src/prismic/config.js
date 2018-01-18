aaa2bd8633113ba46c2d28f01779cb8d7a038f38
Rich Field <rfield@fraedom.com>
2018-01-18 20:18:45 +1300

    Example of using Prismic headless data (#304)
    * example of using prismic
    
    * fix lint errors caused by vscode format on save
    
    * fix line length

---
+++ examples/basic-prismic/src/prismic/config.js
@@ -0,0 +1,3 @@
+// replace this with your own endpoint
+const apiEndpoint = 'https://react-static-blog.prismic.io/api/v2'
+export default apiEndpoint
---
+++ examples/basic-prismic/static.config.js
@@ -0,0 +1,38 @@
+import fetchData from './src/prismic/fetch'
+
+export default {
+  getSiteProps: () => ({
+    title: 'React Static Prismic',
+  }),
+  getRoutes: async () => {
+    const posts = await fetchData()
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getProps: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getProps: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+}
