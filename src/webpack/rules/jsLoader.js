46e3c7a3feeb2ea0cdda8f5f84ea85dbb242286f
Tanner Linsley <tannerlinsley@gmail.com>
2018-02-05 17:03:22 -0700

    Do not compact babel on production, just let uglify do that

--- src/webpack/rules/jsLoader.js
+++ src/webpack/rules/jsLoader.js
@@ -1,10 +1,16 @@
-export default function ({ config }) {
+export default function ({ config, stage }) {
   return {
     test: /\.(js|jsx)$/,
     exclude: config.paths.NODE_MODULES,
     use: [
       {
         loader: 'babel-loader',
+        options:
+          stage === 'production'
+            ? {
+              compact: false,
+            }
+            : {},
       },
     ],
   }
