5081a0aa2131022e739c41fb916e5c0060f63117
Tanner Linsley <tannerlinsley@gmail.com>
2018-02-15 15:01:09 -0700

    Pass tests

--- examples/algolia/src/App.js
+++ examples/algolia/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Router, Link } from "react-static";
-import { hot } from "react-hot-loader";
+import React from 'react'
+import { Router, Link } from 'react-static'
+import { hot } from 'react-hot-loader'
 //
-import Routes from "react-static-routes";
+import Routes from 'react-static-routes'
 
-import "./app.css";
+import './app.css'
 
 const App = () => (
   <Router>
@@ -20,6 +20,6 @@ const App = () => (
       </div>
     </div>
   </Router>
-);
+)
 
-export default hot(module)(App);
+export default hot(module)(App)
