3dc2fddf11125fea2490f5729914a3f84f20edb1
Jonarod <jonrodd@gmail.com>
2018-03-09 20:01:25 +0100

    Adding new example on-the-fly-routing

---
+++ examples/on-the-fly routing/static.config.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default {
+  getSiteData: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    return [
+      {
+        path: '/'
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+}
