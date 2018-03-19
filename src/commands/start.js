150ba55fe92454aee66be2a19bd3084b1a635c96
whmountains <8680147+whmountains@users.noreply.github.com>
2018-03-18 21:09:17 -0300

    Serve directly from public.

--- src/commands/start.js
+++ src/commands/start.js
@@ -40,13 +40,6 @@ export default async function start ({ config, isCLI, debug, silent = !isCLI } =
     siteData,
   })
 
-  // Copy the public directory over
-  if (!silent) console.log('')
-  if (!silent) console.log('=> Copying public directory...')
-  console.time(chalk.green('=> [\u2713] Public directory copied'))
-  copyPublicFolder(config)
-  console.timeEnd(chalk.green('=> [\u2713] Public directory copied'))
-
   // Build the dynamic routes file (react-static-routes)
   if (!silent) console.log('=> Building Routes...')
   console.time(chalk.green('=> [\u2713] Routes Built'))
