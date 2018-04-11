61b7c0d67cea9d9f697d80a4976be5318123bdc7
Tanner Linsley <tannerlinsley@gmail.com>
2018-04-11 11:32:56 -0600

    Fix custom-routing route paths

--- examples/custom-routing/src/App.js
+++ examples/custom-routing/src/App.js
@@ -20,8 +20,8 @@ const App = () => (
       <div className="content">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/blog" component={Blog} />
+          <Route path="about" component={About} />
+          <Route path="blog" component={Blog} />
           <Route component={NotFound} />
         </Switch>
       </div>
