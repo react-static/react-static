11ff7ff8f574c1afb82a304aa5883c11e2c86b0e
Jason Lafferty <jason@gocardless.com>
2018-04-12 10:53:42 +0100

    Add jest & enzyme setup and configuration

---
+++ setupTests.js
@@ -0,0 +1,7 @@
+import 'raf/polyfill'
+
+const Enzyme = require('enzyme')
+const EnzymeAdapter = require('enzyme-adapter-react-16')
+
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() })
