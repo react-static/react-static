eeb7d23945fb43640e78c610b6299d5cc9599696
Chris <chrisnlott@gmail.com>
2018-05-04 02:53:19 -0500

    Update Tailwind example
    * Only extract css to file during one build stage

--- examples/tailwindcss/static.config.js
+++ examples/tailwindcss/static.config.js
@@ -7,96 +7,62 @@ import tailwindcss from 'tailwindcss'
 
 export default {
   webpack: (config, { stage, defaultLoaders }) => {
-    let cssLoader = {}
+    let loaders = [
+      {
+        loader: 'css-loader',
+        options: {
+          minimize: stage !== 'dev',
+          sourceMap: stage === 'dev',
+          importLoaders: 1,
+        },
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          // Necessary for external CSS imports to work
+          // https://github.com/facebookincubator/create-react-app/issues/2677
+          sourceMap: true,
+          ident: 'postcss',
+          plugins: () => [
+            postcssFlexbugsFixes,
+            autoprefixer({
+              browsers: [
+                '>1%',
+                'last 4 versions',
+                'Firefox ESR',
+                'not ie < 9', // React doesn't support IE8 anyway
+              ],
+              flexbox: 'no-2009',
+            }),
+            tailwindcss(path.resolve(__dirname, './tailwind.config.js')),
+          ],
+        },
+      },
+    ]
 
     if (stage === 'dev') {
-      cssLoader = {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              importLoaders: 1,
-            },
+      loaders = ['style-loader'].concat(loaders)
+    } else if (stage === 'prod') {
+      loaders = ExtractTextPlugin.extract({
+        fallback: {
+          loader: 'style-loader',
+          options: {
+            sourceMap: false,
+            hmr: false,
           },
-          {
-            loader: 'postcss-loader',
-            options: {
-              // Necessary for external CSS imports to work
-              // https://github.com/facebookincubator/create-react-app/issues/2677
-              sourceMap: true,
-              ident: 'postcss',
-              plugins: () => [
-                postcssFlexbugsFixes,
-                autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9', // React doesn't support IE8 anyway
-                  ],
-                  flexbox: 'no-2009',
-                }),
-                tailwindcss(path.resolve(__dirname, './tailwind.config.js')),
-              ],
-            },
-          },
-        ],
-      }
-    } else {
-      cssLoader = {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: {
-            loader: 'style-loader',
-            options: {
-              sourceMap: false,
-              hmr: false,
-            },
-          },
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1,
-                minimize: true,
-                sourceMap: false,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                // Necessary for external CSS imports to work
-                // https://github.com/facebookincubator/create-react-app/issues/2677
-                sourceMap: true,
-                ident: 'postcss',
-                plugins: () => [
-                  postcssFlexbugsFixes,
-                  autoprefixer({
-                    browsers: [
-                      '>1%',
-                      'last 4 versions',
-                      'Firefox ESR',
-                      'not ie < 9', // React doesn't support IE8 anyway
-                    ],
-                    flexbox: 'no-2009',
-                  }),
-                  tailwindcss(path.resolve(__dirname, './tailwind.config.js')),
-                ],
-              },
-            },
-          ],
-        }),
-      }
+        },
+        use: loaders,
+      })
     }
 
     config.module.rules = [
       {
         oneOf: [
           defaultLoaders.jsLoader,
-          cssLoader,
+          {
+            test: /\.css$/,
+            use: loaders,
+          },
           defaultLoaders.fileLoader,
         ],
       },
