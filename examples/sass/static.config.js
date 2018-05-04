e12598cd06420241dc8abae596bd703f453bc8cb
Chris <chrisnlott@gmail.com>
2018-05-03 20:00:03 -0500

    Don't extract text plugin during node build process
    fixes #588

--- examples/sass/static.config.js
+++ examples/sass/static.config.js
@@ -37,30 +37,47 @@ export default {
     ]
   },
   webpack: (config, { defaultLoaders, stage }) => {
+    let loaders = []
+
+    if (stage === 'dev') {
+      loaders = [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+    } else {
+      loaders = [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            minimize: stage === 'prod',
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: { includePaths: ['src/'] },
+        },
+      ]
+
+      // Don't extract css to file during node build process
+      if (stage !== 'node') {
+        loaders = ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              sourceMap: false,
+              hmr: false,
+            },
+          },
+          use: loaders,
+        })
+      }
+    }
+
     config.module.rules = [
       {
         oneOf: [
           {
             test: /\.s(a|c)ss$/,
-            use:
-              stage === 'dev'
-                ? [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
-                : ExtractTextPlugin.extract({
-                  use: [
-                    {
-                      loader: 'css-loader',
-                      options: {
-                        importLoaders: 1,
-                        minimize: true,
-                        sourceMap: false,
-                      },
-                    },
-                    {
-                      loader: 'sass-loader',
-                      options: { includePaths: ['src/'] },
-                    },
-                  ],
-                }),
+            use: loaders,
           },
           defaultLoaders.cssLoader,
           defaultLoaders.jsLoader,
