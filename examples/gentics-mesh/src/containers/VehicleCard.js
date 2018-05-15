534be10858930172433722842a04e73175251dea
Rafael Cordones <rafael@cordones.me>
2018-05-15 20:33:04 +0200

    Improve VehicleCard

--- examples/gentics-mesh/src/containers/VehicleCard.js
+++ examples/gentics-mesh/src/containers/VehicleCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import * as PropTypes from 'prop-types'
 import { Link } from 'react-static'
-import { Card, CardImg, CardText, CardBody,
+import { Card, CardImg, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap'
-import renderHTML from 'react-render-html'
 import Dotdotdot from 'react-dotdotdot'
+import stripHtml from 'string-strip-html';
 import NumberFormat from 'react-number-format'
 
 class VehicleCard extends React.Component {
@@ -14,12 +14,15 @@ class VehicleCard extends React.Component {
   render () {
     const vehicle = this.props.product
     return (
-      <Card>
-        <CardImg top width="100%" src={`https://demo.getmesh.io/api/v1/demo/webroot/${vehicle.fields.vehicleImage.path}`} alt={vehicle.fields.name} />
+      <Card className="mb-4 box-shadow">
+        <CardImg top width="100%" src={`https://demo.getmesh.io/api/v1/demo/webroot/${vehicle.fields.vehicleImage.path}?w=350&h=225&crop=fp`} alt={vehicle.fields.name} />
         <CardBody>
           <CardTitle><Link to={vehicle.path}>{vehicle.fields.name}</Link></CardTitle>
           <CardSubtitle><NumberFormat value={vehicle.fields.price} displayType={'text'} thousandSeparator={true} suffix={'EUR'} /></CardSubtitle>
-          <CardText>{renderHTML(vehicle.fields.description)}</CardText>
+          <Dotdotdot clamp={4} tagName="p" className="card-text">
+            {stripHtml(vehicle.fields.description)}
+          </Dotdotdot>
+          <Button className="btn btn-sm btn-outline-primary"><Link to={vehicle.path}>View</Link></Button>
         </CardBody>
       </Card>
     )
