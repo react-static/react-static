0bc2f0aeccebabbdb8191f4a6d0e8c0b3ee71fc7
Jason Lafferty <jasonlafferty@users.noreply.github.com>
2018-05-16 04:29:20 +0100

    Add tests for getConfig (#561)

--- src/static/getConfig.js
+++ src/static/getConfig.js
@@ -3,212 +3,216 @@
 import React from 'react'
 import path from 'path'
 import chokidar from 'chokidar'
-//
+
 import { pathJoin } from '../utils/shared'
 import { reloadRoutes } from './webpack'
+import getDirname from '../utils/getDirname'
 
-//
+const REGEX_TO_CUT_TO_ROOT = /(\..+?)\/.*/g
+const REGEX_TO_REMOVE_TRAILING_SLASH = /^\/{0,}/g
+const REGEX_TO_REMOVE_LEADING_SLASH = /\/{0,}$/g
 
-const defaultEntry = 'index.js'
-const path404 = '404'
+const DEFAULT_NAME_FOR_STATIC_CONFIG_FILE = 'static.config.js'
+// the default static.config.js location
+const DEFAULT_PATH_FOR_STATIC_CONFIG = path.resolve(
+  path.join(process.cwd(), DEFAULT_NAME_FOR_STATIC_CONFIG_FILE)
+)
+const DEFAULT_ROUTES = [{ path: '/' }]
+const DEFAULT_ENTRY = 'index.js'
+const PATH_404 = '404'
 
-// Retrieves the static.config.js from the current project directory
-export default function getConfig (customConfig, { watch } = {}) {
-  if (typeof customConfig === 'string') {
-    // return a custom config file location
-    return fromFile(customConfig)
-  } else if (customConfig) {
-    // return a custom config obj
-    return build(customConfig)
-  }
+export const cutPathToRoot = (string = '') =>
+  string.replace(REGEX_TO_CUT_TO_ROOT, '$1')
 
-  // Return from the default static.config.js location
-  return fromFile(path.resolve(path.join(process.cwd(), 'static.config.js')))
+export const trimLeadingAndTrailingSlashes = (string = '') =>
+  string
+    .replace(REGEX_TO_REMOVE_TRAILING_SLASH, '')
+    .replace(REGEX_TO_REMOVE_LEADING_SLASH, '')
 
-  function fromFile (configPath) {
-    let config
+export const throwErrorIfRouteIsMissingPath = route => {
+  const { path, is404 = false } = route
 
-    const buildConfig = () => {
-      const filename = path.resolve(configPath)
-      delete require.cache[filename]
-      try {
-        return build(require(configPath).default)
-      } catch (err) {
-        console.error(err)
-        return config
-      }
-    }
-
-    config = buildConfig()
-
-    if (watch) {
-      chokidar.watch(configPath).on('all', () => {
-        Object.assign(config, buildConfig())
-        reloadRoutes()
-      })
-    }
-
-    return config
-  }
-
-  function build (config) {
-    const { disableDuplicateRoutesWarning } = config
-
-    // path defaults
-    config.paths = {
-      root: path.resolve(process.cwd()),
-      src: 'src',
-      dist: 'dist',
-      devDist: 'tmp/dev-server',
-      public: 'public',
-      ...(config.paths || {}),
-    }
-
-    // Use the root to resolve all other relative paths
-    const resolvePath = relativePath => path.resolve(config.paths.root, relativePath)
-
-    // Resolve all paths
-    const distPath =
-      process.env.REACT_STATIC_ENV === 'development'
-        ? resolvePath(config.paths.devDist || config.paths.dist)
-        : resolvePath(config.paths.dist)
-    const paths = {
-      ROOT: config.paths.root,
-      LOCAL_NODE_MODULES: path.resolve(__dirname, '../../node_modules'),
-      SRC: resolvePath(config.paths.src),
-      DIST: distPath,
-      PUBLIC: resolvePath(config.paths.public),
-      NODE_MODULES: resolvePath('node_modules'),
-      PACKAGE: resolvePath('package.json'),
-      HTML_TEMPLATE: path.join(distPath, 'index.html'),
-      STATIC_DATA: path.join(distPath, 'staticData'),
-    }
-
-    // Cut siteRoot to the suffix, no trailing slashes
-    const siteRoot = config.siteRoot ? config.siteRoot.replace(/(\..+?)\/.*/g, '$1') : ''
-
-    // Cut siteRoot to the suffix, no trailing slashes
-    const stagingSiteRoot = config.stagingSiteRoot
-      ? config.stagingSiteRoot.replace(/(\..+?)\/.*/g, '$1')
-      : ''
-
-    // Trim basePath of leading and trailing slashes
-    const basePath = config.basePath
-      ? config.basePath.replace(/\/{0,}$/g, '').replace(/^\/{0,}/g, '')
-      : ''
-
-    // Trim stagingBasePath of leading and trailing slashes
-    const stagingBasePath = config.stagingBasePath
-      ? config.stagingBasePath.replace(/\/{0,}$/g, '').replace(/^\/{0,}/g, '')
-      : ''
-
-    // Trim basePath of leading and trailing slashes
-    const devBasePath = config.devBasePath
-      ? config.devBasePath.replace(/\/{0,}$/g, '').replace(/^\/{0,}/g, '')
-      : ''
-
-    const getRoutes = config.getRoutes
-      ? async (...args) => {
-        const routes = await config.getRoutes(...args)
-        return normalizeRoutes(routes, { disableDuplicateRoutesWarning })
-      }
-      : async () =>
-      // At least ensure the index page is defined for export
-        normalizeRoutes(
-          [
-            {
-              path: '/',
-            },
-          ],
-          { disableDuplicateRoutesWarning }
-        )
-
-    const finalConfig = {
-      // Defaults
-      entry: path.join(paths.SRC, defaultEntry),
-      getSiteData: () => ({}),
-      renderToHtml: (render, Comp) => render(<Comp />),
-      prefetchRate: 3,
-      disableDuplicateRoutesWarning: false,
-      disableRouteInfoWarning: false,
-      disableRoutePrefixing: false,
-      outputFileRate: 10,
-      // Config Overrides
-      ...config,
-      // Materialized Overrides
-      paths,
-      siteRoot: siteRoot || '',
-      stagingSiteRoot: stagingSiteRoot || '',
-      basePath: basePath || '',
-      stagingBasePath: stagingBasePath || '',
-      devBasePath: devBasePath || '',
-      extractCssChunks: config.extractCssChunks || false,
-      inlineCss: config.inlineCss || false,
-      getRoutes,
-    }
-
-    // Set env variables to be used client side
-    process.env.REACT_STATIC_PREFETCH_RATE = finalConfig.prefetchRate
-    process.env.REACT_STATIC_DISABLE_ROUTE_INFO_WARNING = finalConfig.disableRouteInfoWarning
-    process.env.REACT_STATIC_DISABLE_ROUTE_PREFIXING = finalConfig.disableRoutePrefixing
-
-    return finalConfig
+  if (!is404 && !path) {
+    throw new Error(`No path defined for route: ${JSON.stringify(route)}`)
   }
 }
 
-// Normalize routes with parents, full paths, context, etc.
-export function normalizeRoutes (routes, config = {}) {
-  const { tree, disableDuplicateRoutesWarning, force404 } = { force404: true, ...config }
-  const flatRoutes = []
+const consoleWarningForRouteWithoutNoIndex = route =>
+  route.noIndex &&
+  console.warn(
+    `=> Warning: Route ${
+      route.path
+    } is using 'noIndex'. Did you mean 'noindex'?`
+  )
 
-  const recurse = (route, parent = { path: '/' }) => {
-    const originalPath = route.is404 ? path404 : pathJoin(route.path)
-    const routePath = route.is404 ? path404 : pathJoin(parent.path, route.path)
+export const createNormalizedRoute = (route, parent = {}, config = {}) => {
+  const { children, ...routeWithOutChildren } = route
+  const { path: parentPath = '/' } = parent
+  const { tree: keepRouteChildren = false } = config
+  const { is404 = false } = route
 
-    if (typeof route.noIndex !== 'undefined') {
-      console.log(`=> Warning: Route ${route.path} is using 'noIndex'. Did you mean 'noindex'?`)
-      route.noindex = route.noIndex
-    }
+  throwErrorIfRouteIsMissingPath(route)
 
-    const normalizedRoute = {
-      ...route,
-      originalPath,
-      path: routePath,
-      noindex: typeof route.noindex === 'undefined' ? parent.noindex : route.noindex,
-      hasGetProps: !!route.getData,
-    }
+  const routePath = is404 ? PATH_404 : pathJoin(parentPath, route.path)
 
-    if (!normalizedRoute.path) {
-      throw new Error('No path defined for route:', normalizedRoute)
-    }
+  consoleWarningForRouteWithoutNoIndex(route)
 
-    if (normalizedRoute.children) {
-      normalizedRoute.children = route.children.map(d => recurse(d, normalizedRoute))
-    }
-
-    if (!tree) {
-      delete normalizedRoute.children
-    }
-
-    const found = flatRoutes.find(d => d.path === normalizedRoute.path)
-
-    if (!found) {
-      flatRoutes.push(normalizedRoute)
-    } else if (!disableDuplicateRoutesWarning) {
-      console.warn('More than one route is defined for path:', route.path)
-    }
-
-    return normalizedRoute
+  const normalizedRoute = {
+    ...(keepRouteChildren ? route : routeWithOutChildren),
+    path: routePath,
+    noindex: route.noindex || parent.noindex || route.noIndex,
+    hasGetProps: !!route.getData,
   }
 
-  const normalizedRoutes = routes.map(d => recurse(d))
+  return normalizedRoute
+}
 
-  if (force404 && !flatRoutes.find(d => d.is404)) {
+export const consoleWarningForMutlipleRoutesWithTheSamePath = routes => {
+  routes.forEach(route => {
+    const found = routes.filter(r => r.path === route.path)
+    if (found.length > 1) {
+      console.warn('More than one route is defined for path:', route.path)
+    }
+  })
+}
+
+const recurseCreateNormalizedRoute = (routes = [], parent, config) =>
+  routes.reduce((memo = [], route) => {
+    const normalizedRoute = createNormalizedRoute(route, parent, config)
+    const routeExists = memo.find(({ path }) => path === normalizedRoute.path)
+
+    return [
+      ...memo,
+      ...recurseCreateNormalizedRoute(route.children, normalizedRoute, config),
+      ...(routeExists ? [] : [normalizedRoute]),
+    ]
+  }, [])
+
+// Normalize routes with parents, full paths, context, etc.
+export const normalizeRoutes = (routes, config = {}) => {
+  const { disableDuplicateRoutesWarning, force404 = true } = config
+  const flatRoutes = recurseCreateNormalizedRoute(routes, {}, config)
+
+  if (!disableDuplicateRoutesWarning) {
+    consoleWarningForMutlipleRoutesWithTheSamePath(flatRoutes)
+  }
+
+  if (force404 && !flatRoutes.find(r => r.is404)) {
     flatRoutes.push({
       is404: true,
-      path: path404,
+      path: PATH_404,
     })
   }
 
-  return tree ? normalizedRoutes : flatRoutes
+  return flatRoutes
+}
+
+// At least ensure the index page is defined for export
+export const makeGetRoutes = config => async (...args) => {
+  const { getRoutes = async () => DEFAULT_ROUTES } = config
+  const routes = await getRoutes(...args)
+  return normalizeRoutes(routes, config)
+}
+
+export const buildConfigation = (config = {}) => {
+  // path defaults
+  config.paths = {
+    root: path.resolve(process.cwd()),
+    src: 'src',
+    dist: 'dist',
+    devDist: 'tmp/dev-server',
+    public: 'public',
+    ...(config.paths || {}),
+  }
+
+  // Use the root to resolve all other relative paths
+  const resolvePath = relativePath =>
+    path.resolve(config.paths.root, relativePath)
+
+  // Resolve all paths
+  const distPath =
+    process.env.REACT_STATIC_ENV === 'development'
+      ? resolvePath(config.paths.devDist || config.paths.dist)
+      : resolvePath(config.paths.dist)
+
+  const paths = {
+    ROOT: config.paths.root,
+    LOCAL_NODE_MODULES: path.resolve(getDirname(), '../../node_modules'),
+    SRC: resolvePath(config.paths.src),
+    DIST: distPath,
+    PUBLIC: resolvePath(config.paths.public),
+    NODE_MODULES: resolvePath('node_modules'),
+    PACKAGE: resolvePath('package.json'),
+    HTML_TEMPLATE: path.join(distPath, 'index.html'),
+    STATIC_DATA: path.join(distPath, 'staticData'),
+  }
+
+  // Defaults
+  const finalConfig = {
+    // Defaults
+    entry: path.join(paths.SRC, DEFAULT_ENTRY),
+    getSiteData: () => ({}),
+    renderToHtml: (render, Comp) => render(<Comp />),
+    prefetchRate: 3,
+    disableRouteInfoWarning: false,
+    disableRoutePrefixing: false,
+    outputFileRate: 10,
+    // Config Overrides
+    ...config,
+    // Materialized Overrides
+    paths,
+    siteRoot: cutPathToRoot(config.siteRoot, '$1'),
+    stagingSiteRoot: cutPathToRoot(config.stagingSiteRoot, '$1'),
+    basePath: trimLeadingAndTrailingSlashes(config.basePath),
+    stagingBasePath: trimLeadingAndTrailingSlashes(config.stagingBasePath),
+    devBasePath: trimLeadingAndTrailingSlashes(config.devBasePath),
+    extractCssChunks: config.extractCssChunks || false,
+    inlineCss: config.inlineCss || false,
+    getRoutes: makeGetRoutes(config),
+  }
+
+  // Set env variables to be used client side
+  process.env.REACT_STATIC_PREFETCH_RATE = finalConfig.prefetchRate
+  process.env.REACT_STATIC_DISABLE_ROUTE_INFO_WARNING =
+    finalConfig.disableRouteInfoWarning
+  process.env.REACT_STATIC_DISABLE_ROUTE_PREFIXING =
+    finalConfig.disableRoutePrefixing
+
+  return finalConfig
+}
+
+const buildConfigFromPath = configPath => {
+  const filename = path.resolve(configPath)
+  delete require.cache[filename]
+  try {
+    const config = require(configPath).default
+    return buildConfigation(config)
+  } catch (err) {
+    return {}
+  }
+}
+
+const fromFile = (configPath = DEFAULT_PATH_FOR_STATIC_CONFIG, watch) => {
+  const config = buildConfigFromPath(configPath)
+
+  if (watch) {
+    chokidar.watch(configPath).on('all', () => {
+      Object.assign(config, buildConfigFromPath(configPath))
+      reloadRoutes()
+    })
+  }
+
+  return config
+}
+
+// Retrieves the static.config.js from the current project directory
+export default function getConfig (customConfig, { watch } = {}) {
+  if (typeof customConfig === 'object') {
+    // return a custom config obj
+    return buildConfigation(customConfig)
+  }
+
+  // return a custom config file location
+  // defaults to constant paath for static config
+  return fromFile(customConfig, watch)
 }
