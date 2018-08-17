b2472aff54b8ec562ffd7d2772b25f49b2c3e7b8
Robert Schönthal <caziel@gmx.net>
2018-08-17 11:11:38 +0800

    fix building app to some folder outside app-root (#707)

--- src/static/webpack/rules/jsLoader.js
+++ src/static/webpack/rules/jsLoader.js
@@ -1,6 +1,31 @@
-import babelPreset from '../../../../babel-preset';
+import fs from "fs";
+
+// we check which babel config file exists in the project root
+const readBabelConfig = (root) => {
+  const babelFiles = [`${root}/.babelrc`, `${root}/.babelrc.js`,`${root}/.babel.config.js`];
+
+  let extendsFile = {};
+
+  babelFiles.forEach(file => {
+    try {
+      fs.statSync(file);
+      extendsFile = {extends: file}
+    }
+    catch(err) {
+      // dont do anything
+    }
+  });
+
+  return extendsFile;
+}
 
 export default function({ config, stage }) {
+  let babelFile = {};
+
+  if (config.paths.DIST.indexOf(config.paths.ROOT) !== 0) {
+    babelFile = readBabelConfig(config.paths.ROOT);
+  }
+
   return {
     test: /\.(js|jsx)$/,
     exclude: new RegExp(`(node_modules|${config.paths.EXCLUDE_MODULES})`),
@@ -8,6 +33,8 @@ export default function({ config, stage }) {
       {
         loader: 'babel-loader',
         options: {
+          ...babelFile,
+          root: config.paths.ROOT,
           presets: [[babelPreset, { modules: false }]],
           cacheDirectory: stage !== 'prod',
         },
