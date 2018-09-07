537a23823ccef1be022e504dbeb8fdb9d21f37dc
Mahmoud "Max" Wardeh <m@lurnid.com>
2018-09-07 16:14:48 +0100

    Add Contenful CMS example

---
+++ examples/contentful/src/contenful/config.js
@@ -0,0 +1,6 @@
+export const SPACE_ID = ""
+
+// Content Delivery API access token
+export const CDAPI_ACCESS_TOKEN = "" 
+
+export const POST_CONTENT_TYPE_ID = "blogPost"
---
+++ examples/contentful/static.config.js
@@ -0,0 +1,40 @@
+import fetchPosts from './src/contenful/fetchPosts'
+
+export default {
+
+  getSiteData: () => ({
+    title: 'React Static with Contentful CMS',
+  }),
+  getRoutes: async () => {
+    const posts = await fetchPosts()
+    
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Home',
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        path: '/blog',
+        component: 'src/containers/Blog',
+        getData: () => ({
+          posts
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.slug}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+}
