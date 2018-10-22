f29a8ce899adfc6eed0df8521074867a9f1742fa
Kevin Cooper <cooperka92@gmail.com>
2018-10-22 16:33:56 -0400

    Update yarn integrity + prettier (#830)
    * Commit yarn 1.10 'integrity' info
    
    * Commit automatic prettier changes

--- packages/react-static/src/utils/__tests__/chunkBuilder.test.js
+++ packages/react-static/src/utils/__tests__/chunkBuilder.test.js
@@ -1,24 +1,32 @@
-import {chunkNameFromFile, absoluteToRelativeChunkName} from '../chunkBuilder';
+import { chunkNameFromFile, absoluteToRelativeChunkName } from '../chunkBuilder'
 
-describe("utils/chunkBuilder", () => {
-    describe("chunkNameFromFile", () => {
-        it("can generate chunkNames from relative files", () => {
-            expect(chunkNameFromFile("bazz/component.jsx")).toBe("bazz-component");
-        });
-    
-        it("can generate chunkNames from absolute files", () => {
-            expect(chunkNameFromFile("/bar/bazz/component.jsx")).toBe("bar-bazz-component");
-        });    
-    });
-
-    describe("absoluteToRelativeChunkName", () => {
-        it("can make absolute chunk names relative", () => {
-            expect(absoluteToRelativeChunkName("/foo/bar/bazz/", "foo-bar-bazz-src-component.jsx")).toBe("src-component")
-        })
-    
-        it("leaves relative chunk names untouched", () => {
-            expect(absoluteToRelativeChunkName("/foo/bar/bazz/", "src-bar")).toBe("src-bar")
-        })    
+describe('utils/chunkBuilder', () => {
+  describe('chunkNameFromFile', () => {
+    it('can generate chunkNames from relative files', () => {
+      expect(chunkNameFromFile('bazz/component.jsx')).toBe('bazz-component')
     })
 
+    it('can generate chunkNames from absolute files', () => {
+      expect(chunkNameFromFile('/bar/bazz/component.jsx')).toBe(
+        'bar-bazz-component'
+      )
+    })
+  })
+
+  describe('absoluteToRelativeChunkName', () => {
+    it('can make absolute chunk names relative', () => {
+      expect(
+        absoluteToRelativeChunkName(
+          '/foo/bar/bazz/',
+          'foo-bar-bazz-src-component.jsx'
+        )
+      ).toBe('src-component')
+    })
+
+    it('leaves relative chunk names untouched', () => {
+      expect(absoluteToRelativeChunkName('/foo/bar/bazz/', 'src-bar')).toBe(
+        'src-bar'
+      )
+    })
+  })
 })
