6c027c30e9c2eebf4c08de20841dbfd47e12e80b
Tanner Linsley <tannerlinsley@gmail.com>
2018-12-11 11:20:40 -0700

    copyExamples logic

--- packages/react-static/src/copyExamples.js
+++ packages/react-static/src/copyExamples.js
@@ -5,37 +5,37 @@ import { exec } from 'child_process'
 const examples = path.resolve(__dirname, '../../../examples/')
 const libExamples = path.resolve(__dirname, '../libExamples/')
 
-// async function init() {
-//   await fs.remove(libExamples)
-//   fs.copy(examples, libExamples, {
-//     filter: file => {
-//       return ![
-//         'tmp',
-//         'dist',
-//         '.DS_Store',
-//         'yarn.lock',
-//         '.log',
-//         '.tgz',
-//         'node_modules',
-//         '.history',
-//       ].some(d => file.includes(d))
-//     },
-//   })
-// }
-
 async function init() {
-  const files = await fs.readdir(examples)
-  files
-    .filter(d => !d.startsWith('.'))
-    .forEach(file => {
-      exec(
-        `cd ${path.resolve(
-          __dirname,
-          '../../../examples/',
-          file
-        )} && yarn add react@latest react-dom@latest`
-      )
-    })
+  await fs.remove(libExamples)
+  fs.copy(examples, libExamples, {
+    filter: file => {
+      return ![
+        'tmp',
+        'dist',
+        '.DS_Store',
+        'yarn.lock',
+        '.log',
+        '.tgz',
+        'node_modules',
+        '.history',
+      ].some(d => file.includes(d))
+    },
+  })
 }
+
+// async function init() {
+//   const files = await fs.readdir(examples)
+//   files
+//     .filter(d => !d.startsWith('.'))
+//     .forEach(file => {
+//       exec(
+//         `cd ${path.resolve(
+//           __dirname,
+//           '../../../examples/',
+//           file
+//         )} && yarn add react@latest react-dom@latest`
+//       )
+//     })
+// }
 
 init()
