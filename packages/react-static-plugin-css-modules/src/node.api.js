d3001d3dfe592018107b16386de42a3ddf3430d5
zackjackson <zackary.l.jackson@gmail.com>
2018-12-12 23:27:47 -0800

    passing options through to css loader

--- packages/react-static-plugin-css-modules/src/node.api.js
+++ packages/react-static-plugin-css-modules/src/node.api.js
@@ -1,30 +1,32 @@
 import ExtractCssChunks from 'extract-css-chunks-webpack-plugin'
 
-export default ({ includePaths = [], ...rest }) => ({
+export default (options = {}) => ({
   webpack: (config, { stage }) => {
     let loaders = []
 
     const styleLoader = { loader: 'style-loader' }
     const cssLoader = {
       loader: 'css-loader',
-      options: {
+      options: Object.assign({
         modules: true,
         importLoaders: 1,
-      },
-    }
+      }, options),
+    };
 
     if (stage === 'dev') {
       // Dev
       loaders = [styleLoader, cssLoader]
     } else if (stage === 'node') {
-      cssLoader.loader = 'css-loader/locals'
+      loaders = [
+        { ...cssLoader, loader: 'css-loader/locals' },
+      ]
     } else {
       // Prod
       loaders = [ExtractCssChunks.loader, cssLoader]
     }
 
     config.module.rules[0].oneOf.unshift({
-      test: /\.css$/,
+      test: /\.s(a|c)ss$/,
       use: loaders,
     })
 
