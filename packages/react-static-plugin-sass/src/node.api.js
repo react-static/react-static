c7a00dafd83562e36aa215231b66decbb5c4bc64
Robert Schönthal <caziel@gmx.net>
2018-12-18 04:09:49 +1300

    fix css-loader 2.0 version breaks (removed minimize option) (#884)

--- packages/react-static-plugin-sass/src/node.api.js
+++ packages/react-static-plugin-sass/src/node.api.js
@@ -1,4 +1,5 @@
 import ExtractCssChunks from 'extract-css-chunks-webpack-plugin'
+import semver from 'semver';
 
 export default ({ includePaths = [], ...rest }) => ({
   webpack: (config, { stage }) => {
@@ -15,7 +16,6 @@ export default ({ includePaths = [], ...rest }) => ({
       loader: 'css-loader',
       options: {
         importLoaders: 1,
-        minimize: stage === 'prod',
         sourceMap: false,
       },
     }
@@ -29,6 +29,14 @@ export default ({ includePaths = [], ...rest }) => ({
       loaders = [cssLoader, sassLoader]
     } else {
       // Prod
+
+      // for legacy css-loader version (<2.0) we need to add "minimize" to minify css code
+      // for >2.0 it is handled with https://github.com/NMFR/optimize-css-assets-webpack-plugin
+      const cssLoaderVersion = require('css-loader/package.json').version;
+      if (semver.satisfies(cssLoaderVersion, '<2') === true) {
+        cssLoader.options.minimize = true;
+      }
+
       loaders = [ExtractCssChunks.loader, cssLoader, sassLoader]
     }
 
