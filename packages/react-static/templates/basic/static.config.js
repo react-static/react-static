224aff2123b3496fb5dcd53b109940becf3fa1e4
Tanner Linsley <tannerlinsley@gmail.com>
2019-01-03 15:42:33 -0700

    Revert template change

--- packages/react-static/templates/basic/static.config.js
+++ packages/react-static/templates/basic/static.config.js
@@ -4,33 +4,24 @@ export default {
   getSiteData: () => ({
     title: 'React Static',
   }),
-  getRoutes: async ({ stage, incremental }) => {
+  getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
     )
-    return incremental
-      ? [
-          {
-            path: '/blog',
-            getData: () => ({
-              posts: [{}],
-            }),
-          },
-        ]
-      : [
-          {
-            path: '/blog',
-            getData: () => ({
-              posts,
-            }),
-            children: posts.map(post => ({
-              path: `/post/${post.id}`,
-              component: 'src/containers/Post',
-              getData: () => ({
-                post,
-              }),
-            })),
-          },
-        ]
+    return [
+      {
+        path: '/blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+    ]
   },
 }
