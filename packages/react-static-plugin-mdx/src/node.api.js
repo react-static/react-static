a4860f45b9eab6c5782d69d68c26b9045067643c
Matteo Frana <46195306+matteofrana@users.noreply.github.com>
2019-01-16 14:39:56 +0100

    Added MDX plugin (#969)
    * Added mdx plugin
    
    * Moved files other than node.api.js out of /src
    
    * Use babel loader from defaultLoaders.jsLoader; take the src path from jsLoader.include
    
    * Updated readme with routes generation
    
    * Use extensions from plugin options
    
    * Update README.md

---
+++ packages/react-static-plugin-mdx/src/node.api.js
@@ -0,0 +1,19 @@
+export default ({
+  includePaths = [],
+  extensions = ['.md', '.mdx']
+}) => ({
+  config: config => {
+    config.extensions = [...config.extensions, ...extensions]
+  },
+  webpack: (webpackConfig, { stage, defaultLoaders }) => {
+    const mdxLoaderPath = require.resolve('@mdx-js/loader')
+
+    webpackConfig.module.rules[0].oneOf.unshift({
+      test: /.mdx?$/,
+      include: [defaultLoaders.jsLoader.include, ...includePaths],
+      use: [defaultLoaders.jsLoader.use[0], mdxLoaderPath],
+    })
+
+    return webpackConfig
+  },
+})
