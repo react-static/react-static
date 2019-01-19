030add284781b2c16ca211feda0bf2d6a46a8c52
James Messinger <james.messinger@gmail.com>
2019-01-19 10:27:20 -0600

    Added a test that uses a ".jsx" extension instead of ".js" for the "static.config" file

---
+++ packages/react-static/src/static/__mocks__/static.config.mock.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default {
+  Document: ({ Html, Head, Body, children }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+}
