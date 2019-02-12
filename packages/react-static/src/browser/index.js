4ae2a759b6d649ddd19e148103126b3ff72819a1
Krzysztof Pyrzyk <krzysztof.pyrzyk@airhelp.com>
2019-02-12 09:14:28 +0100

    Check if routeInfo exists before accesing its property

--- packages/react-static/src/browser/index.js
+++ packages/react-static/src/browser/index.js
@@ -285,7 +285,7 @@ export async function prefetchTemplate(path, { priority } = {}) {
     templateErrorByPath[path] = true
     return
   }
-  if (!routeInfo.templateLoaded && Template.preload) {
+  if (routeInfo && !routeInfo.templateLoaded && Template.preload) {
     if (priority) {
       await Template.preload()
     } else {
