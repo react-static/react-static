257f2e0efcdfcdca019177ea5b76f50a032da56d
Ben Edwards <ben.edwards@clock.co.uk>
2019-04-15 11:56:19 +0100

    Removes invalid closing `</xml>` tag
    Closes #1117

--- packages/react-static-plugin-sitemap/src/buildXML.js
+++ packages/react-static-plugin-sitemap/src/buildXML.js
@@ -91,8 +91,7 @@ export function generateXML(
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${xmlRoutes}
-    </urlset>
-    </xml>`
+    </urlset>`
 }
 
 export function getPermaLink(path, prefixPath) {
