3cdc76c96b5d696ac2309e7406aba9e5ced37412
Tanner Linsley <tannerlinsley@gmail.com>
2019-04-16 06:03:24 -0600

    Update buildXML.js

--- packages/react-static-plugin-sitemap/src/buildXML.js
+++ packages/react-static-plugin-sitemap/src/buildXML.js
@@ -92,8 +92,7 @@ export function generateXML(
     `<?xml version="1.0" encoding="UTF-8"?>`,
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
     xmlRoutes,
-    `</urlset>`,
-    `</xml>`
+    `</urlset>`
   ].join(staging ? '\n' : '')
 }
 
