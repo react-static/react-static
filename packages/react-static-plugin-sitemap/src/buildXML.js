932796b5d9aab03405fd926839adbd39993dc35a
tannerlinsley <tannerlinsley@gmail.com>
2019-04-16 06:22:31 -0600

    Prettier Format

--- packages/react-static-plugin-sitemap/src/buildXML.js
+++ packages/react-static-plugin-sitemap/src/buildXML.js
@@ -92,7 +92,7 @@ export function generateXML(
     `<?xml version="1.0" encoding="UTF-8"?>`,
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
     xmlRoutes,
-    `</urlset>`
+    `</urlset>`,
   ].join(staging ? '\n' : '')
 }
 
