e890a2f4ca12a022c2cf75d672bb8a77ab3a4f96
jxom <jakemoxey@gmail.com>
2019-07-09 10:42:17 +1000

    Migrate from overrideBrowserslist to browserslist config

--- packages/react-static-plugin-less/src/node.api.js
+++ packages/react-static-plugin-less/src/node.api.js
@@ -29,12 +29,6 @@ export default ({ includePaths = [], ...rest }) => ({
         plugins: () => [
           postcssFlexbugsFixes,
           autoprefixer({
-            overrideBrowserslist: [
-              '>1%',
-              'last 4 versions',
-              'Firefox ESR',
-              'not ie < 9', // React doesn't support IE8 anyway
-            ],
             flexbox: 'no-2009',
           }),
         ],
--- packages/react-static/src/static/webpack/rules/cssLoader.js
+++ packages/react-static/src/static/webpack/rules/cssLoader.js
@@ -21,12 +21,6 @@ function initCSSLoader() {
         plugins: () => [
           postcssFlexbugsFixes,
           autoprefixer({
-            overrideBrowserslist: [
-              '>1%',
-              'last 4 versions',
-              'Firefox ESR',
-              'not ie < 9', // React doesn't support IE8 anyway
-            ],
             flexbox: 'no-2009', // I'd opt in for this - safari 9 & IE 10.
           }),
         ],
