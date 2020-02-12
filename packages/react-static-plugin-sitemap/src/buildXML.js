ce46b9c2854d9564ec2c1d7cde50ba426a6679ae
Peter A. Tariche <ptariche@gmail.com>
2020-02-11 16:11:27 -0800

    simplified url set xmlns as full sitemaps require too many changes as feature

--- packages/react-static-plugin-sitemap/src/buildXML.js
+++ packages/react-static-plugin-sitemap/src/buildXML.js
@@ -89,7 +89,7 @@ export function generateXML(
 
   return [
     `<?xml version="1.0" encoding="UTF-8"?>`,
-    `<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`,
     xmlRoutes,
     `</urlset>`,
   ].join(staging ? '\n' : '')
