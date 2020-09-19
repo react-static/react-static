1ce14a210572e9aaa180fcb98b2cf1304423e00b
Rostyslav Khanas <rtkhanas@gmail.com>
2020-09-19 12:03:46 +0300

    Add instructions how to make it work with preact

--- packages/react-static/src/bootstrapApp.js
+++ packages/react-static/src/bootstrapApp.js
@@ -4,6 +4,9 @@ import { staticInfoContext } from './browser/hooks/useStaticInfo'
 import Suspense from './Suspense'
 
 // Override the suspense module to be our own
+// This is expected to break when using preact
+// In order to make it work with preact 10, use `patch-package` to remove those 2 lines
+// Reference: https://github.com/react-static/react-static/pull/1500
 React.Suspense = Suspense
 React.default.Suspense = Suspense
 
